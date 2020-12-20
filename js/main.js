5e63c7b35ef7d5ec424756e35a4b539dccaf4efa
Merizza Khan <Merizza@users.noreply.github.com>
2020-12-20 12:11:21 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ window.addEventListener('load',
     const serviceIsTrue = URLPathname.split("/")[1];
     const authService = URLPathname.split("/")[2];
 
-    const processNameRawText1 = URLPathname.slice(9, ).replace(/_+/g, " ");
-    const processNameRawText2 = URLPathname.slice(12, ).replace(/_+/g, " ");
+    const processNameRawText1 = URLPathname.split("/")[2].replace(/_+/g, " ");
+    const processNameRawText2 = URLPathname.split("/")[3].replace(/_+/g, " ");
 	
 	//const dummyText1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	//const dummyText2 = "Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.";
