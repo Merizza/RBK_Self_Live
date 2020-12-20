8a37078287aaaee0b03eb372ee13ede4de166701
Merizza Khan <Merizza@users.noreply.github.com>
2020-12-20 11:23:05 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ window.addEventListener('load',
   function() { 
     
     /*--------------Fetch URL-----------------*/
-    const fetchURL = window.location.href;
-    const processURL = new URL(fetchURL.toString());
-    const URLPathname = processURL.pathname;
+    const URLPathname = window.location.pathname;
     
-	/*--------------Extracting the name of the Process from the URL--------------*/
+    /*--------------Extracting the name of the Process from the URL--------------*/
     const serviceIsTrue = URLPathname.split("/")[1];
     const authService = URLPathname.split("/")[2];
 
@@ -16,7 +14,7 @@ window.addEventListener('load',
 	//const dummyText1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	//const dummyText2 = "Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.";
 	
-	/*-----------------Redirect Homepage Menus to Main Website-----------------------*/
+    /*-----------------Redirect Homepage Menus to Main Website-----------------------*/
     (function redirectToMain() {
       const myServiceLink = document.getElementsByClassName("btn btn-lg btn-primary top");
 	  /*--------Redirect Default Main Menu to Main Site--------------*/
@@ -70,7 +68,7 @@ window.addEventListener('load',
         
         elem.insertAdjacentHTML("beforeend", "<a style='font-weight: bold;'class='btn btn-lg btn-primary top'>"+processNameRawText2+"</a>");
         smNavElement.insertAdjacentHTML("beforeend", "<li><a href='/MyRequests' tabindex='0'>My Requests</a></li>");
-		smNavElement.insertAdjacentHTML("beforeend", "<li><a href='#' tabindex='0' style='font-weight:bold;'>"+processNameRawText2+"</a></li>");
+				smNavElement.insertAdjacentHTML("beforeend", "<li><a href='#' tabindex='0' style='font-weight:bold;'>"+processNameRawText2+"</a></li>");
 		
 		 /*--------------------Registration Page------------------------------*/
 		var loginScreenAnon = document.querySelector("#anonymousLoginScreen>h1");
